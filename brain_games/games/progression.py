028f9a28baf12ee9c132aaa513da1865005fa862
SHArtyom <sartiom1@gmail.com>
2022-11-25 00:16:01 +0300

    brain-progression algorithm fix, distrib updated

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,7 +8,7 @@ def get_rules():
 def generate():
     start_number = randint(0, 20)
     inc_number = randint(0, 20)
-    progression = [(start_number + i * inc_number) for i in range(1, 10)]
+    progression = [(start_number + i * inc_number) for i in range(10)]
     hidden_number = randint(0, 9)
     answer = str(progression[hidden_number])
     progression[hidden_number] = '..'
